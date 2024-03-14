454679fe137185d640a20f6ccc7f7cae301ed8e3
Mayara Alhessa <eng.mayaraalhessa@gmail.com>
2024-03-14 14:13:43 +0000

    Criando o arquivo de operadores aritméticos

---
+++ op-aritmeticos.js
@@ -0,0 +1,10 @@
+const numero1 = 20;
+const numero2 = 40;
+// copiar linhas alt + shift p baixo
+
+console.log(`Os números das operações são: ${numero1} e ${numero2}`);
+console.log(`A soma dos números é: ${numero1 + numero2}`);
+console.log(`A subtração dos números é: ${numero1 - numero2}`);
+console.log(`A multiplicação dos números é: ${numero1 * numero2}`);
+console.log(`A divisao dos números é: ${numero1 / numero2}`);
+console.log(`O resto dos números é: ${numero1 % numero2}`);
